7dccdd02c69cf3f57998bf6aa0fab133a56e0d13
hyejinki <zlagg23@gmail.com>
2024-04-09 19:38:07 +0900

    루돌프의 반란

--- codetree/Rudolph's_Rebellion.py
+++ codetree/Rudolph's_Rebellion.py
@@ -1,126 +1,109 @@
 N, M, P, C, D = map(int, input().split())
-x, y = map(int, input().split())
-ru = [x, y]
-santa_dict = dict()
-
+ri, rj = map(lambda x:int(x) - 1, input().split())
+point = [0] * (P+1)
+alive = [1] * (P+1)
+alive[0] = 0
+sleep = [1] * (P+1)
+santa = [[N]*2 for _ in range(P+1)]
 for _ in range(P):
     n, i, j = map(int, input().split())
-    santa_dict[n] = [(i, j), -1, 1]
-point = [0] * (P+1)
+    santa[n] = [i-1, j-1]
 
 
-# 거리 구하기
-def cal_distance(r1, c1, r2, c2):
-    return (r1 - r2)**2 + (c1 - c2)**2
+def push_santa(r1, c1, dr, dc, p, idx):
+    # 만났어
+    q = []
+    q.append((r1, c1, dr, dc, p, idx))
+    sleep[idx] = turn + 2
+
+    while q:
+        ci, cj, dr, dc, p, idx = q.pop(0)
+        ni, nj = ci + dr*p, cj + dc*p
+        index = idx
+        if 0<=ni<N and 0<=nj<N and [ni, nj] in santa:
+            for i in range(1, P+1):
+                if alive[i] == 1 and santa[i] == [ni, nj]:
+                    index = i
+                    break
+            q.append((ni, nj, dr, dc, 1, index))
+            santa[idx] = [ni, nj]
+
+        elif 0<=ni<N and 0<=nj<N:
+            santa[idx] = [ni, nj]
 
 
-def comp(x, y):
-    if x - y > 0:
-        return -1
-    elif x - y < 0:
-        return 1
-    else:
-        return 0
-
-
-
-def push(ci, cj, oi, oj):
-
-     for k in santa_dict:
-         if (ci, cj) == santa_dict[k][0]:
-             if 1 <= ci + oi <= N and 1 <= cj + oj <= N:
-                 santa_dict[k][0] = (ci + oi, cj + oj)
-                 santa_dict[k][1] = turn
-             else:
-                 santa_dict[k][2] = 0
-
-
-def move_ru(ri, rj, si, sj, C, idx):
-    di, dj = comp(ri, si), comp(rj, sj)
-    ni, nj = ri + di, rj + dj
-
-    ru[0], ru[1] = ni, nj
-    if (ni, nj) == (si, sj):
-        # 점수
-        point[idx] += C
-        #밀기
-        pi, pj = si + di*C, sj + dj*C
-        if 1 <= pi <= N and 1<= pj<=N:
-            santa_dict[idx][0] = (pi, pj)
-            santa_dict[idx][1] = turn
         else:
-            santa_dict[idx][2] = 0
-
-        push(pi, pj, di, dj)
-
-
-
-
-def move_santa(ri, rj, si, sj, D, idx):
-    minx = cal_distance(ri, rj, si, sj)
-    cij = (si, sj)
-    ij = (0, 0)
-    for (di, dj) in ((-1, 0), (0, 1), (1, 0), (0, -1)):
-        ni, nj = si+di, sj + dj
-        if 1<=ni<=N and 1<=nj<=N:
-            for s in santa_dict:
-                if santa_dict[s][0] == (ni, nj):
-                    continue
-                x = cal_distance(ri, rj, ni, nj)
-                if x<minx:
-                    minx = x
-                    cij = (ni, nj)
-                    ij = (di, dj)
-
-    if cij == (ri, rj):
-        # 점수
-        point[idx] += D
-        #밀기
-        pi, pj = cij[0] - ij[0]*D, cij[1] - ij[1]*d
-
-        if 1 <= pi <= N and 1<= pj<=N:
-            santa_dict[idx][0] = (pi, pj)
-            santa_dict[idx][1] = turn
-        else:
-            santa_dict[idx][2] = 0
-
-        push(cij[0], cij[1], ij[0], ij[1])
-
-        if santa_dict[idx]:
-            santa_dict[idx][0] = (cij[0], cij[1])
-
-
+            print(idx)
+            alive[idx] = 0
 
 for turn in range(1, M+1):
-    flag = 0
-    for idx in santa_dict:
-        if santa_dict[idx][2] == 1:
-            flag = 1
-    if flag == 0:
+    if alive.count(1) == 0:
         break
 
-    ri, rj = ru[0], ru[1]
+    # 루돌푸 움직임
+    minx = []
     minV = N*N*2
-    mi, mj = N+1, N+1
-    for idx in santa_dict:
-        if santa_dict[idx][2] == 1:
-            if turn - santa_dict[idx][1] >= 2:
-                (si, sj) = santa_dict[idx][0]
-                d = cal_distance(ri, rj, si, sj)
-                if d < minV or (d == minV and (si, sj) > (mi, mj)):
-                    minV, mi, mj, i = d, si, sj, idx
+    for idx in range(1, P+1):
+        if alive[idx]==0 : continue
+        si, sj = santa[idx]
+        dist = (ri - si)**2 + (rj - sj)**2
+        if dist < minV:
+            minx = [(si, sj, idx)]
+            minV = dist
+        elif dist == minV:
+            minx.append((si, sj, idx))
+    minx.sort(reverse=True)
 
-    move_ru(ri, rj, mi, mj, C, i) #루돌푸 움직임
+    si, sj, mnidx = minx[0]
 
-    for idx in santa_dict:
-        if santa_dict[idx][2] == 1:
-            if turn - santa_dict[idx][1] >= 2:
-                (si, sj) = santa_dict[idx][0]
-                move_santa(ru[0], ru[1], si, sj, D, idx)
+    # 방향
+    di, dj = 0, 0
+    if ri - si > 0:
+        di = -1
+    elif ri - si < 0:
+        di = 1
+
+    if rj - sj > 0:
+        dj = -1
+    elif rj - sj <0:
+        dj = 1
+
+    ni, nj = ri + di, rj + dj
+
+    if (ni, nj) == (si, sj):
+        point[mnidx] += C
+        push_santa(ni, nj, di, dj,C, mnidx)
+
+    else:
+        ri, rj = ni, nj
 
 
-    for idx in santa_dict:
-        if santa_dict[idx][2] == 1:
-            point[idx] += 1
+    # 산타 움직임
+    for idx in range(1, P+1):
+        if alive[idx] == 0:    continue
+        if sleep[idx] > turn:   continue
+        si, sj = santa[idx]
+        dist = (ri - si)**2 + (rj - sj)**2
 
-print(point)
+        can = []
+        for dsi, dsj in ((-1, 0), (0, 1), (1, 0), (0, -1)):
+            nsi, nsj = si + dsi, sj + dsj
+            new_dist = (ri - nsi)**2 + (rj - nsj)**2
+            if 0<=nsi<N and 0<=nsj<N and new_dist < dist and [nsi, nsj] not in santa:
+                can.append([nsi, nsj, dsi, dsj])
+                dist = new_dist
+
+        if len(can) == 0:   continue
+        sr, sc, dr, dc = can[-1]
+
+        if (ri, rj) == (sr, sc):
+            point[idx] += D
+            push_santa(sr, sc, -dr, -dc, D, idx)
+        else:
+            santa[idx] = [sr, sc]
+
+    for i in range(1, P+1):
+        if alive[i] == 1:
+            point[i] += 1
+
+print(*point[1:])
